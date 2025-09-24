98a02e8904ad1ef99774e5d0c66749781dcb47c3
kokchun <kokchungiang@gmail.com>
2025-09-24 11:17:32 +0200

    intial react structure

---
+++ cool_app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Detta är min super awesome coola swayiga app</h1>
+        <p>Why do programmers hate light, because they attract bugs</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
